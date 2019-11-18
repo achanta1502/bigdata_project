4f2cf31748a6b0cbab7b445ec41280542cc02a86
achanta1502 <achanta_pavankumar@hotmail.com>
2019-11-18 02:55:06 -0600

    Twitter kafka stream
    kafka stream
    
    twitter kafka stream
    
    few changes

--- project/spark.py
+++ project/spark.py
@@ -1,15 +1,15 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.streaming import StreamingContext
-
-import json
-import re
-from textblob import TextBlob
+from pyspark.sql import SQLContext, SparkSession
+from pyspark.streaming.kafka import KafkaUtils
 from elasticsearch import Elasticsearch
-from datetime import datetime
 
-TCP_IP = 'localhost'
-TCP_PORT = 9001
+import argparse
 
+IP = 'localhost'
+PORT = '9092'
+LOG_LEVEL = 'WARN'
+TOPIC = ''
 
 def connection():
     # Pyspark
@@ -18,26 +18,57 @@ def connection():
     conf.setAppName('TwitterApp')
     conf.setMaster('local[2]')
 
-    conf.set("spark.network.timeout","4200s")
-    conf.set("spark.executor.heartbeatInterval","4000s")
+    conf.set("spark.network.timeout", "4200s")
+    conf.set("spark.executor.heartbeatInterval", "4000s")
     # create spark context with the above configuration
     sc = SparkContext(conf=conf)
+    sc.setLogLevel(LOG_LEVEL)
+    sqlcontext = SQLContext(sc)
+    spark = SparkSession(sc)
     # create the Streaming Context from spark context with interval size 2 seconds
-    ssc = StreamingContext(sc, 4)
+    ssc = StreamingContext(sc, 1)
     ssc.checkpoint("checkpoint_TwitterApp")
     # creating an elastic search port for data to be sent to kibana
     es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-    data_stream = ssc.socketTextStream(TCP_IP, TCP_PORT)
-    return ssc, es, data_stream
+    return ssc, es
 
+
+def get_sql_context_instance(sparkContext):
+    if 'sqlContextSingletonInstance' not in globals():
+        globals()['sqlContextSingletonInstance'] = SQLContext(sparkContext)
+    return globals()['sqlContextSingletonInstance']
+
+
+def get_spark_session_instance(sparkContext):
+    if 'sparkSessionSingletonInstance' not in globals():
+        globals()['sparkSessionSingletonInstance'] = SparkSession(sparkContext)
+    return globals()['sparkSessionSingletonInstance']
+
+
+def process(time, rdd):
+    print("===========%s=========" % str(time))
+    try:
+        #process the rdd
+        pass
+    except Exception as e:
+        print(e)
+        pass
 
 def start():
     # create config and start streaming
-    ssc, es, data_stream = connection()
+    ssc, es = connection()
+    kafka_stream = KafkaUtils.createDirectStream(ssc, [TOPIC], {"metadata.broker.list": "localhost:9092"})
+    kafka_stream.foreachRDD(process)
     # your processing here
     ssc.start()
     ssc.awaitTermination()
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--topic', default='twitter', dest='topic',
+                        help='topic where kakfa can publish messages')
+
+    args = parser.parse_args()
+    TOPIC = args.topic
     start()
--- project/stream.py
+++ project/stream.py
@@ -1,58 +1,78 @@
-import tweepy
-import socket
-import sys
+from tweepy import Stream
+from tweepy import StreamListener
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+from kafka import KafkaProducer
 
-# twitter app token key and consumer key
+import pickle
+import argparse
+
+
+HASH_TAG = ''
+TOPIC = ''
+
+IP = 'localhost'
+PORT = '9092'
+producer = None
+
 ACCESS_TOKEN = ''
 ACCESS_SECRET = ''
 CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 
-HASHTAG = ''
-
-TCP_IP = 'localhost'
-TCP_PORT = 9001
-
-# create sockets
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.connect((TCP_IP, TCP_PORT))
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
-conn, addr = s.accept()
+def set_kafka():
+    global producer
+    producer = KafkaProducer(bootstrap_servers=['%s:%s' % (IP, PORT)])
 
 
-class MyStreamListener(tweepy.StreamListener):
+def credentials_load():
+    # twitter app token key and consumer key
+    data = pickle.load(open("creds.p", 'rb'))
+    global ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET
+    ACCESS_TOKEN = data['ACCESS_TOKEN']
+    ACCESS_SECRET = data['ACCESS_SECRET']
+    CONSUMER_KEY = data['CONSUMER_KEY']
+    CONSUMER_SECRET = data['CONSUMER_SECRET']
 
-    @classmethod
-    def on_status(cls, status):
-        print(status.text)
-        conn.send(status.text.encode('utf-8'))
 
-    @classmethod
-    def on_error(cls, status_code):
-        if status_code == 420:
-            return False
-        else:
-            print(status_code)
+def authenticate():
+    credentials_load()
+    auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    return auth
+
+
+class MyStreamListener(StreamListener):
+    def __int__(self):
+        pass
+
+    def on_data(self, data):
+        # Producer produces data for consumer
+        producer.send(TOPIC, data.encode('utf-8'))
+        print(data)
+        return True
+
+    def on_error(self, status):
+        # Status printing
+        print(status)
+        return True
 
 
 def stream():
-    my_stream = tweepy.Stream(auth=auth, listener=MyStreamListener())
-
-    my_stream.filter(track=[HASHTAG])
+    set_kafka()
+    my_stream = Stream(auth=authenticate(), listener=MyStreamListener())
+    my_stream.filter(track=HASH_TAG)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 0:
-        if len(sys.argv) != 6:
-            print("Arguments are not correctly defined")
-            exit(2)
-        ACCESS_TOKEN = sys.argv[1]
-        ACCESS_SECRET = sys.argv[2]
-        CONSUMER_KEY = sys.argv[3]
-        CONSUMER_SECRET = sys.argv[4]
-        HASHTAG = sys.argv[5]
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--hashtag', default='#accident', dest='hashtag',
+                        help='hashtag of the twitter app to retrieve tweets')
+    parser.add_argument('--topic', default='twitter', dest='topic',
+                        help='topic where kakfa can publish messages')
+
+    args = parser.parse_args()
+    HASH_TAG = args.hashtag
+    TOPIC = args.topic
     stream()
